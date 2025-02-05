fd394db386c2bf168e19edeeb83bef49253e8931
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-02-05 01:13:25 -0300

    Create Card Class

---
+++ My project/Assets/Scripts/Card.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[System.Serializable]
+public class Card : MonoBehaviour
+{
+    public int cardId;
+    public string cardName;
+    public string cardDescription;
+}
